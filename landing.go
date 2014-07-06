3712dcf1a22d2b997a71af700abfc0b87dcf7491
Charles Southerland <charlie@stuphlabs.com>
2014-07-06 00:53:08 -0500

    Adding simple landing page.

---
+++ landing.go
@@ -0,0 +1,18 @@
+package scaledown
+
+import (
+	"github.com/fitstar/falcore"
+	"net/http"
+)
+
+func NewLandingFilter() falcore.RequestFilter {
+	return falcore.NewRequestFilter(
+		func(req *falcore.Request) *http.Response {
+			return falcore.StringResponse(
+				req.HttpRequest,
+				200,
+				nil,
+				"<html><head><title>Pullcord Landing Page</title></head><body><h1>Pullcord Landing Page</h1><p>This is the landing page for Pullcord, a reverse proxy for cloud-based web apps that allows the servers the web apps run on to be turned off when not in use.</p><p>If you are unsure of how to proceed, please contact the site administrator.</p></body></html>")
+		})
+}
+
---
+++ landing_test.go
@@ -0,0 +1,30 @@
+package scaledown
+
+import (
+	"github.com/fitstar/falcore"
+	"net/http"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLandingPage(t *testing.T) {
+	request, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+	_, response := falcore.TestWithRequest(request, NewLandingFilter(), nil)
+	assert.Equal(t, 200, response.StatusCode)
+}
+
+func TestAnotherLandingPage(t *testing.T) {
+	request, err := http.NewRequest("GET", "/other/page/somewhere/else.php", nil)
+	assert.NoError(t, err)
+	_, response := falcore.TestWithRequest(request, NewLandingFilter(), nil)
+	assert.Equal(t, 200, response.StatusCode)
+}
+
+func TestPostLandingPage(t *testing.T) {
+	request, err := http.NewRequest("POST", "/", nil)
+	assert.NoError(t, err)
+	_, response := falcore.TestWithRequest(request, NewLandingFilter(), nil)
+	assert.Equal(t, 200, response.StatusCode)
+}
+
