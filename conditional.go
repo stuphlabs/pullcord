8a568903357fa3483ebe790e651f7245a683adf7
Charles Southerland <charlie@stuphlabs.com>
2016-03-06 11:25:42 -0600

    Added error reporting to the conditional filter, and cleaned up the unit tests (issue-#32) in order to properly test this new change.

--- conditional.go
+++ conditional.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 )
 
-func NewConditionalFilter(test func(req *falcore.Request) bool, onTrue falcore.RequestFilter, onFalse falcore.RequestFilter) falcore.RequestFilter {
+func NewConditionalFilter(test func(req *falcore.Request) (bool, error), onTrue falcore.RequestFilter, onFalse falcore.RequestFilter, onError falcore.RequestFilter) falcore.RequestFilter {
 	return falcore.NewRequestFilter(
 		func(req *falcore.Request) *http.Response {
-			if test(req) {
+			test_true, err := test(req)
+			if err != nil {
+				return onError.FilterRequest(req)
+			} else if test_true {
 				return onTrue.FilterRequest(req)
 			} else {
 				return onFalse.FilterRequest(req)
