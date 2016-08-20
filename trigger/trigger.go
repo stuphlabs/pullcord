b05e0ac906d4cf7d109d2fa3214ee8fb4ed6be40
Charles Southerland <charlie@stuphlabs.com>
2016-08-19 23:00:43 -0500

    Adding a basic shell command based trigger.

---
+++ trigger/trigger.go
@@ -0,0 +1,20 @@
+package trigger
+
+import (
+	// "github.com/stuphlabs/pullcord"
+)
+
+// TriggerHandler is an abstract interface describing a system which provides
+// triggers that can be called based on certain events (like a service being
+// detected as down, an amount of time passing without a service being
+// accessed, etc.). For the moment, the only trigger mechanism provided is a
+// simple string being passed in, and although additional trigger mechanisms
+// could be added later, it is not yet clear that others would be necessary.
+//
+// TriggerString is a function that provides a trigger mechanism based on an
+// arbitrary string. A minimal amount of additional structure could be achieved
+// through the use of serialization format like JSON.
+type TriggerHandler interface {
+	TriggerString(message string) (err error)
+}
+
