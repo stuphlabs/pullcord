fabfe27f3b79dad9bee4b286fe33205f2e150daa
Charles Southerland <charlie@stuphlabs.com>
2016-12-06 20:56:45 -0600

    Removing another duplicated test helper.

--- trigger/delay_test.go
+++ trigger/delay_test.go
@@ -1,26 +1,11 @@
 package trigger
 
 import (
-	"errors"
 	"github.com/stretchr/testify/assert"
 	//"github.com/stuphlabs/pullcord"
 	"testing"
 	"time"
 )
-
-type counterTriggerHandler struct {
-	name string
-	count uint
-}
-
-func (th *counterTriggerHandler) TriggerString(name string) error {
-	if name == th.name {
-		th.count += 1
-		return nil
-	} else {
-		return errors.New("bad error string")
-	}
-}
 
 func TestDelayTriggerSingleDelay(t *testing.T) {
 	testString := "testing"
