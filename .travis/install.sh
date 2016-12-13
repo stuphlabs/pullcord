5d59997125f8a91839602175303e60fddac66051
Charles Southerland <charlie@stuphlabs.com>
2016-12-13 00:49:46 -0600

    Adding proidiot/gone to Travis setup.

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,9 +2,11 @@
 
 echo "Installing dependencies...\n"
 
-go get -v github.com/stretchr/testify/assert \
-	github.com/fitstar/falcore \
+go get -v \
 	github.com/dustin/randbo \
+	github.com/fitstar/falcore \
+	github.com/proidiot/gone \
+	github.com/stretchr/testify/assert \
 	golang.org/x/crypto \
 	golang.org/x/net/html
 
