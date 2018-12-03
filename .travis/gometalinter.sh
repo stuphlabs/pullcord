52878b74dd21e59658b62853d2ae41cc0b0d07d0
Charles Southerland <charlie@stuphlabs.com>
2018-12-03 00:14:04 -0600

    Get Travis to run gometalinter, but don't yet error out on its failure

--- .travis/gometalinter.sh
+++ .travis/gometalinter.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+IGNORE_ERRORS='TRUE'
+
 go get -t -u -v github.com/alecthomas/gometalinter
 
 gometalinter -i -u
@@ -9,12 +11,18 @@ gometalinter -i -u
 go get -t -u -v ./...
 
 echo "Running gometalinter (this could take some time)..."
+if [ "x${IGNORE_ERRORS}" = 'xTRUE' ]; then
+	set +e
+fi
 gometalinter \
 	--enable-all \
 	--deadline=600s \
 	--enable-gc \
 	--cyclo-over=15 \
 	./...
+LINT_EXIT=$?
 
-echo "No gometalinter issues found."
+if [ ${LINT_EXIT} -eq 0 ]; then
+	echo "No gometalinter issues found."
+fi
 
